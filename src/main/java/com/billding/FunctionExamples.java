21447f3765fe4f0486382ff5cbee600bc29d8281
bfrasure <bill.frasure@gmail.com>
2019-03-17 13:44:29 -0600

    Start speccing out star formation functions/classes

--- src/main/java/com/billding/FunctionExamples.java
+++ src/main/java/com/billding/FunctionExamples.java
@@ -11,32 +11,68 @@ public class FunctionExamples {
     private class User {}
     private class Commit {}
     private class Json<T> {}
+    private class URI {}
 
+    // Should the request actually dictate return types?
+    // Or does the user only know the URL, and it's up to the server to decide response format?
+    // The Client at least *expects* a certain format.
 
-    public Lesson predicateExamples(
+    public Lesson functionComposition(
         Function<Click, HttpRequest<Json<List<Commit>>>> frontEndCode,
         Function<HttpRequest<?>, User> getAuthenticatedUser,
         Function<User, List<Commit>> retrieveCommitsForUser,
-        Function<Commit, Json<Commit>> serializeCommit,
-        // Getting shaky here
-        Function<List<Json<Commit>>, Json<List<Commit>>> someFunctionalTransformer
+        Function<List<Commit>, Json<List<Commit>>> listJsonSerializer
+    ) {
+
+        Function<Click, Json<List<Commit>>> gitUserActivity =
+            frontEndCode
+                .andThen(getAuthenticatedUser)
+                .andThen(retrieveCommitsForUser)
+                .andThen(listJsonSerializer)
+            ;
+
+        return new Lesson(EnumSet.of(Topic.Function, Topic.Composition));
+    }
+
+    private class SimulationParameters {}
+    private class Particle {}
+    private class Octree {}
+    private class Simulation {}
+    private class VideoOutput {}
+
+    public Lesson starFormation(
+        Function<SimulationParameters, List<Particle>> particleGenerator
+
+
+    ) {
+        Function<SimulationParameters, VideoOutput> fullProgram;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    private void figureOutThisMonadTransformer(
+        Function<List<Json<Commit>>, Json<List<Commit>>> someFunctionalTransformer,
+        Function<Commit, Json<Commit>> serializeCommit
     ) {
 
         Function<List<Commit>, Json<List<Commit>>> listJsonSerializer =
-             commits ->
+            commits ->
                 someFunctionalTransformer
                     .apply(
-                            commits
-                                .stream()
-                                .map(serializeCommit)
-                                .collect(Collectors.toList()));
-        frontEndCode
-            .andThen(getAuthenticatedUser)
-            .andThen(retrieveCommitsForUser)
-            .andThen(listJsonSerializer)
-            ;
-        Function<Click, Json<List<Commit>>> gitUserActivity;
-
-        return new Lesson(EnumSet.of(Topic.Function, Topic.Composition));
+                        commits
+                            .stream()
+                            .map(serializeCommit)
+                            .collect(Collectors.toList()));
     }
 }
